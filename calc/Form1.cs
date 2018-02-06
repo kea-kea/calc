edce77560ac50b968c69e4921a9f111fce93336e
Jon Jon <kea-kea@mail.ru>
2018-02-06 18:39:24 +1000

    2

--- calc/Form1.cs
+++ calc/Form1.cs
@@ -16,5 +16,25 @@ namespace calc
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
